14f759bac7e6c3702adeb6da29042829b1a225c4
Muhammad Waqas <waqasazamkml@gmail.com>
2021-12-18 16:34:13 +0500

    Initial commit

---
+++ app/src/main/java/com/dozygeek/lazyrecipes/RecipeListActivity.java
@@ -0,0 +1,14 @@
+package com.dozygeek.lazyrecipes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_list);
+    }
+}
