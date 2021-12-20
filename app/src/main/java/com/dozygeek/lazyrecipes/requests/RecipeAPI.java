dfbc9741a93bb492680b6bc22057a513bc425ece
Muhammad Waqas <waqasazamkml@gmail.com>
2021-12-20 16:49:43 +0500

    Implemented interface methods inside RecipeAPI. searchRecipe will get list of recipes and getRecipe will get single ID wrt recipe_id passed to it.

--- app/src/main/java/com/dozygeek/lazyrecipes/requests/RecipeAPI.java
+++ app/src/main/java/com/dozygeek/lazyrecipes/requests/RecipeAPI.java
@@ -1,4 +1,26 @@
 package com.dozygeek.lazyrecipes.requests;
 
+import com.dozygeek.lazyrecipes.requests.responses.RecipeResponse;
+import com.dozygeek.lazyrecipes.requests.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 public interface RecipeAPI {
+
+    @GET("recipes")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String api_key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
+
+    @GET("recipes/{recipe_id}")
+    Call<RecipeResponse> getRecipe(
+            @Path("recipe_id") int recipe_id,
+            @Query("key") String api_key
+    );
+
 }
